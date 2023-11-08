7578f35ea735230e402ec1976be957a935114e67
Muhammad Umar Khan <omar.khan@mazikglobal.com>
2023-11-08 04:06:04 -0800

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn -w 2 -k uvicorn.workers.Uvicorn Worker app:app
--- app.py
+++ app.py
@@ -1,34 +1,41 @@
-from flask import Flask, request, jsonify
-from transformers import BertForSequenceClassification, BertTokenizerFast
+from fastapi import FastAPI, HTTPException, Query
+from pydantic import BaseModel
 
-app = Flask(__name__)
+from transformers import BertForSequenceClassification, BertTokenizerFast
+import torch
+
+app = FastAPI()
 
 model_path = "Model"
 model = BertForSequenceClassification.from_pretrained(model_path)
 tokenizer = BertTokenizerFast.from_pretrained(model_path)
 
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    try:
-        data = request.get_json()
-        text = data['text']
+class TextData(BaseModel):
+    text: str
 
-        inputs = tokenizer(text, padding=True, truncation=True, max_length=512, return_tensors="pt").to("cuda")
+
+@app.post('/predict/')
+async def predict(data: TextData):
+    try:
+        text = data.text
+
+        inputs = tokenizer(text, padding=True, truncation=True, max_length=512, return_tensors="pt")
         outputs = model(**inputs)
         probs = outputs.logits.softmax(1)
         pred_label_idx = probs.argmax()
         pred_label = model.config.id2label[pred_label_idx.item()]
 
-        return jsonify({
-            "probs": probs.tolist(),
-            "pred_label_idx": pred_label_idx.item(),
-            "pred_label": pred_label
-        })
+        return {
+
+            "CPT CODE INDEX": pred_label_idx.item(),
+            "Disease CPT CODE:": pred_label
+        }
 
     except Exception as e:
-        return jsonify({"error": str(e)})
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
